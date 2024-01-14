5f2aa4135093e2cb586f72ebf3587eb4a539cf2e
Alexis Williams <Thecomixguy@hotmail.com>
2024-01-14 12:27:27 +0000

    Start `Entity` trait and `Player impl`

--- rayst-engine/src/game.rs
+++ rayst-engine/src/game.rs
@@ -1,14 +1,52 @@
 //! Rayst engine structs to do with the gameplay elements,
 //! like the player, entities, et cetera. 
 
+use std::f32::consts::PI;
+
 use notan::{math::Vec3, prelude::Texture};
+
+/// Entity Trait
+pub trait Entity {
+    /// Get entity position
+    fn get_pos(&self) -> Vec3;
+
+    /// Return angle the entity is facing (degrees)
+    fn get_angle_deg(&self) -> f32;
+    
+    // I thought it would make sense to have the API have an automatically
+    // implemented radians function too, seeing as it is often used in maths
+    // that some developers may need (including internally in Rayst).
+    /// Return angle the entity is facing (radians)
+    fn get_angle_rad(&self) -> f32;
+
+    /// Translate entity by a Vec3
+    fn translate(&mut self, translation: Vec3);
+}
 
 /// Rayst engine Player structure. Stores information
 /// relevant to the exact player.
 pub struct Player {
-    pub pos: Vec3,
-    pub angle: f32,
+    pos: Vec3,
+    angle_deg: f32,
     pub weapon: Box<dyn Weapon>
+}
+
+impl Entity for Player {
+    fn get_pos(&self) -> Vec3 {
+        self.pos    
+    }
+
+    fn translate(&mut self, translation: Vec3) {
+        self.pos = self.pos + translation;
+    }
+
+    fn get_angle_deg(&self) -> f32 {
+        self.angle_deg
+    }
+
+    fn get_angle_rad(&self) -> f32 {
+        (self.angle_deg / 360.0 ) * (2.0 * PI)
+    }
 }
 
 /// Enum used for dictating whether a weapon is
@@ -40,6 +78,5 @@ pub trait Weapon {
     fn fire_animation(&self) -> Animation;
     /// Gets the relevant attack structure
     fn get_attack(&self) -> Attack;
-    /// Adjust player position based off of a vector translation
-    fn translate(&mut self, direction: &Vec3);
 }
+
